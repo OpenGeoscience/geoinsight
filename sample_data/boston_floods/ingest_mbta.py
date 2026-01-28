7f9b6a91bf7b768c32bbd35a8ec4da1d67ce0b84
Anne Haley <anne.haley@kitware.com>
2026-01-28 12:26:32 -0500

    feat: Add postprocessing step to ingest of mbta dataset

---
+++ sample_data/boston_floods/ingest_mbta.py
@@ -0,0 +1,35 @@
+import json
+from geoinsight.core.models import VectorFeature
+
+LINE_COLORS = {
+    'RED': '#D31414',
+    'GREEN': '#188A00',
+    'BLUE': '#1F63AC',
+    'ORANGE': '#D88901',
+    'SILVER': '#7B8B86',
+}
+
+def convert_dataset(dataset, options):
+    # Run standard conversion task first
+    dataset.spawn_conversion_task(
+        layer_options=options.get('layers'),
+        network_options=options.get('network_options'),
+        region_options=options.get('region_options'),
+        asynchronous=False,
+    )
+
+    # Post-processing
+    # For each associated vector feature, add style properties
+    # To color features by the LINE property
+    for feature in VectorFeature.objects.filter(vector_data__dataset=dataset):
+        lines = feature.properties.get('LINE', '').split('/')
+        colors = [LINE_COLORS[line] for line in lines if line in LINE_COLORS]
+        if len(colors):
+            fill = colors[0]
+            stroke = colors[1] if len(colors) > 1 else fill
+            default_style = {
+                'fill': fill,
+                'stroke': stroke,
+            }
+            feature.properties = feature.properties | default_style
+            feature.save()
