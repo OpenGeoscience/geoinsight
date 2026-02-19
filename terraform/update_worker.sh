fbb67be276fafe9e5c04881656eb2e7b89e9c0aa
Anne Haley <anne.haley@kitware.com>
2026-02-19 12:16:46 -0500

    feat: Add script to update celery worker

---
+++ terraform/update_worker.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+HOST="$1"
+if [[ -z "$HOST" ]]; then
+    echo "Usage: $0 <ec2-ip-address>"
+    exit 1
+fi
+
+SSH_TARGET="ubuntu@$HOST"
+
+echo "==> Fetching secrets from Heroku..."
+ENV_FILE=$(mktemp)
+heroku config --app geoinsight --shell > "$ENV_FILE"
+
+echo "==> Uploading env file to remote machine..."
+scp "$ENV_FILE" "$SSH_TARGET:/home/ubuntu/geoinsight.prod.env"
+rm "$ENV_FILE"
+
+echo "==> Adding SOURCE_VERSION to env file..."
+ssh "$SSH_TARGET" 'echo "SOURCE_VERSION=$(git -C /home/ubuntu/geoinsight rev-parse HEAD)" >> /home/ubuntu/geoinsight.prod.env'
+
+echo "==> Pulling changes..."
+ssh "$SSH_TARGET" 'cd /home/ubuntu/geoinsight && git pull'
+
+echo "==> Restarting celery service..."
+ssh "$SSH_TARGET" 'sudo systemctl restart celery'
+
+echo "==> Done! Celery worker is restarted on $HOST"
