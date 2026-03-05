49e3d3bd6cf176ac56de677a03bacf80ec737c85
Anne Haley <anne.haley@kitware.com>
2026-03-05 10:21:09 -0500

    chore: Remove permission class and filter backend from viewset

--- uvdat/core/rest/view_state.py
+++ uvdat/core/rest/view_state.py
@@ -3,12 +3,9 @@ from __future__ import annotations
 from rest_framework.viewsets import ModelViewSet
 
 from uvdat.core.models import ViewState
-from uvdat.core.rest.access_control import GuardianFilter, GuardianPermission
 from uvdat.core.rest.serializers import ViewStateSerializer
 
 
 class ViewStateViewSet(ModelViewSet):
     queryset = ViewState.objects.all()
     serializer_class = ViewStateSerializer
-    permission_classes = [GuardianPermission]
-    filter_backends = [GuardianFilter]
