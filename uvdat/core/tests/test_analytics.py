50f694b84cdf567a19b284941852d2d344c8dfd6
Brian Helba <brian.helba@kitware.com>
2026-03-06 14:40:54 -0500

    Move test and script imports to top level

--- uvdat/core/tests/test_analytics.py
+++ uvdat/core/tests/test_analytics.py
@@ -1,13 +1,20 @@
 from __future__ import annotations
 
+from django.contrib.auth.models import User
 from django.core.management import call_command
 import pytest
+
+from uvdat.core.models import Chart, Dataset, Network
+from uvdat.core.tasks.analytics import (
+    FloodNetworkFailure,
+    FloodSimulation,
+    NetworkRecovery,
+    analysis_types,
+)
 
 
 @pytest.mark.django_db
 def test_rest_list_analysis_types(user, authenticated_api_client, project):
-    from uvdat.core.tasks.analytics import analysis_types
-
     # TODO: remove this when analytics are no longer hidden from non-superusers
     user.is_superuser = True
     user.save()
@@ -52,15 +59,6 @@ def test_rest_run_analysis_task_no_inputs(authenticated_api_client, user, projec
 @pytest.mark.slow
 @pytest.mark.django_db
 def test_flood_analysis_chain(project):
-    from django.contrib.auth.models import User
-
-    from uvdat.core.models import Chart, Dataset, Network
-    from uvdat.core.tasks.analytics import (
-        FloodNetworkFailure,
-        FloodSimulation,
-        NetworkRecovery,
-    )
-
     # ensure a superuser exists
     User.objects.create_superuser("testsuper")
 
